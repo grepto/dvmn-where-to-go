b472496d127702fbf8d786818ff6e301fb71c80b
grepto <grepto@gmail.com>
2020-06-09 14:38:50 +0300

    add model Place

--- src/places/models.py
+++ src/places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField(max_length=200)
+    description_long = models.TextField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    def __str__(self):
+        return self.title
