848e949b45a80d1f25ff7671756f73c0fb041ca7
grepto <grepto@gmail.com>
2020-06-09 19:38:11 +0300

    add preciew image to PlaceImageInline

--- src/places/admin.py
+++ src/places/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.utils.html import mark_safe, format_html
+
 from .models import Place, PlaceImage
+
 
 # admin.site.register(Place)
 # admin.site.register(PlaceImage)
@@ -9,6 +12,15 @@ class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
     verbose_name = 'Фотография'
     verbose_name_plural = 'Фотографии'
+    fields = ['image', 'preview_image', 'sort_order']
+    readonly_fields = ['preview_image', ]
+
+    def preview_image(self, obj):
+        return format_html('<img src="{url}" height={height}/>'.format(
+            url=obj.image.url,
+            height=obj.image.height if obj.image.height <= 200 else 200,
+        )
+        )
 
 
 @admin.register(Place)
