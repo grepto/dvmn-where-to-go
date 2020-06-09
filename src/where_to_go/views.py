748e85050414103b351ff5e36523997048f74f61
grepto <grepto@gmail.com>
2020-06-09 17:54:29 +0300

    Add Json API for places

--- src/where_to_go/views.py
+++ src/where_to_go/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import json
+
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
 from places.models import Place
 
@@ -23,3 +26,20 @@ def show_main_page(request):
     places_geojson = dict(type='FeatureCollection', features=features)
     data = dict(places_geojson=places_geojson)
     return render(request, 'index.html', context=data)
+
+
+def place_detailed_view(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    data = dict(
+        title=place.title,
+        imgs=[img.image.url for img in place.images.all()],
+        description_short=place.description_short,
+        description_long=place.description_long,
+        coordinates=dict(
+            lng=place.longitude,
+            lat=place.latitude
+        )
+    )
+
+    # return JsonResponse(data)
+    return JsonResponse(data, json_dumps_params={'indent': 2, 'ensure_ascii': False})
