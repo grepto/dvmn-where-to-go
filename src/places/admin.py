df78614398eef280473ac7b112c6ee3bbcd88749
grepto <grepto@gmail.com>
2020-06-09 19:00:45 +0300

    Add inline Place images in admin

--- src/places/admin.py
+++ src/places/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
 
-admin.site.register(Place)
-admin.site.register(PlaceImage)
+# admin.site.register(Place)
+# admin.site.register(PlaceImage)
+
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    verbose_name = 'Фотография'
+    verbose_name_plural = 'Фотографии'
+
+
+@admin.register(Place)
+class PLaceAdmin(admin.ModelAdmin):
+    inlines = [
+        PlaceImageInline
+    ]
--- src/places/models.py
+++ src/places/models.py
@@ -2,19 +2,21 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField(max_length=200)
-    description_long = models.TextField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Краткое описание', max_length=200)
+    description_long = models.TextField('Подробное описание')
+    longitude = models.FloatField('Широта')
+    latitude = models.FloatField('Долгота')
 
     def __str__(self):
         return self.title
 
 
 class PlaceImage(models.Model):
-    sort_order = models.IntegerField()
-    image = models.ImageField()
+    verbose_name = 'Фотография'
+    verbose_name_plural = 'Фотографии'
+    image = models.ImageField('Картинка')
+    sort_order = models.IntegerField('Позиция')
     place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images')
 
     def __str__(self):
