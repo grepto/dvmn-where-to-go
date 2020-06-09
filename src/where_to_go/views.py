33716af2901e7df60a8e4e9a18eb0464702251cc
grepto <grepto@gmail.com>
2020-06-09 16:19:40 +0300

    Get geo-json from database

--- src/where_to_go/views.py
+++ src/where_to_go/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
+
+from places.models import Place
 
 
 def show_main_page(request):
-    return render(request, 'index.html')
+    features = [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.pk,
+                'detailsUrl': ''
+            }
+        }
+        for place in Place.objects.all()
+    ]
+
+    places_geojson = dict(type='FeatureCollection', features=features)
+    data = dict(places_geojson=places_geojson)
+    return render(request, 'index.html', context=data)
