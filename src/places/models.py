e957b0e548466f6cbbfe4791f38f9825b276a1c2
grepto <grepto@gmail.com>
2020-06-10 15:31:04 +0300

    fix pylint comments

--- src/places/models.py
+++ src/places/models.py
@@ -3,6 +3,7 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
+    """The model for the place"""
     title = models.CharField('Название', max_length=200, null=False, blank=False)
     description_short = models.TextField('Краткое описание', max_length=200, null=False, blank=False)
     description_long = HTMLField('Подробное описание', null=True, blank=True)
@@ -10,17 +11,18 @@ class Place(models.Model):
     latitude = models.FloatField('Долгота')
 
     def __str__(self):
-        return self.title
+        return str(self.title)
 
 
 class PlaceImage(models.Model):
+    """The model for photo for the place"""
     verbose_name = 'Фотография'
     verbose_name_plural = 'Фотографии'
     image = models.ImageField('Картинка')
     sort_order = models.PositiveIntegerField('Позиция', default=0, blank=False, null=False)
     place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images')
 
-    class Meta(object):
+    class Meta:
         ordering = ['sort_order']
 
     def __str__(self):
