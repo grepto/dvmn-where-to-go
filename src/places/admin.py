e56f707637c927b77740c4425656067474e9e82f
grepto <grepto@gmail.com>
2020-09-06 14:02:34 +0300

    add verbose name for placeimage

--- src/places/admin.py
+++ src/places/admin.py
@@ -8,15 +8,13 @@ from places.models import Place, PlaceImage
 class PlaceImageInline(SortableInlineAdminMixin, admin.TabularInline):
     extra = 0
     model = PlaceImage
-    verbose_name = 'Фотография'
-    verbose_name_plural = 'Фотографии'
     fields = ['image', 'preview_image', 'sort_order']
     readonly_fields = ['preview_image']
 
     def preview_image(self, obj):
         return format_html('<img src="{url}" height={height}/>'.format(
             url=obj.image.url,
-            height=obj.image.height if obj.image.height <= 200 else 200))
+            height=min(obj.image.height, 200)))
 
 
 @admin.register(Place)
--- src/places/models.py
+++ src/places/models.py
@@ -4,11 +4,11 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     """The model for the place"""
-    title = models.CharField('Название', max_length=200, null=False, blank=False)
-    short_description = models.TextField('Краткое описание', max_length=300, null=False, blank=True)
-    long_description = HTMLField('Подробное описание', null=False, blank=True)
-    longitude = models.FloatField('Широта', null=False, blank=True, default=0)
-    latitude = models.FloatField('Долгота', null=False, blank=True, default=0)
+    title = models.CharField('Название', max_length=200)
+    short_description = models.TextField('Краткое описание', max_length=300, blank=True)
+    long_description = HTMLField('Подробное описание', blank=True)
+    longitude = models.FloatField('Широта', blank=True, default=0)
+    latitude = models.FloatField('Долгота', blank=True, default=0)
 
     def __str__(self):
         return str(self.title)
@@ -16,14 +16,15 @@ class Place(models.Model):
 
 class PlaceImage(models.Model):
     """The model for photo for the place"""
-    verbose_name = 'Фотография'
-    verbose_name_plural = 'Фотографии'
+
     image = models.ImageField('Картинка')
-    sort_order = models.PositiveIntegerField('Позиция', default=0, blank=False, null=False)
-    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images')
+    sort_order = models.PositiveIntegerField('Позиция', default=0)
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images', verbose_name='Место')
 
     class Meta:
         ordering = ['sort_order']
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
 
     def __str__(self):
         return f'{self.sort_order} {self.place}'
